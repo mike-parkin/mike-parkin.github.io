f3764036c19c4c4211f215dc9c3445fa9e6088f3
Mike Parkin <michael.parkin1988@gmail.com>
2020-12-17 21:52:24 +1300

    Add JS to make images index clickable

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('tech-link').onclick = function () {
+    window.open("./blog/blog-tech.html","_self");
+}
+document.getElementById('culture-link').onclick = function () {
+    window.open("./blog/blog-culture.html", "_self");
+}
